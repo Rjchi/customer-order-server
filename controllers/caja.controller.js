c6a0c6685edbc0a50973bdddf4b4f7e3635ec559
Rjchi <richardospina18@gmail.com>
2023-09-03 17:22:22 -0500

    modificar controlador de caja

--- controllers/caja.controller.js
+++ controllers/caja.controller.js
@@ -6,9 +6,9 @@ const deleteOrdersByTable = async (req, res) => {
       req.params.table,
     ]);
     if (result.affectedRows !== 0) {
-        return res.status(200);
+        return res.status(200).json({ message: "Eliminado correctamente" });
     }
-    return res.status(204);
+    return res.status(404).json({ message: "No hay pedidos para esa mesa" });
   } catch (error) {
     res.status(500).json({
       message: `Error al eliminar pedidos de una mesa detalles ${error.message}`,
