a666cffb1edfc6c4cd9fee6e5327bf79fe77b07a
Rjchi <richardospina18@gmail.com>
2023-09-19 18:45:20 -0500

    [Chore] Agregar nuevo script de la base de datos de desarrollo

--- database/xampp.sql
+++ database/xampp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-09-2023 a las 00:30:41
+-- Tiempo de generación: 20-09-2023 a las 01:42:06
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 7.4.30
 
@@ -64,7 +64,25 @@ CREATE TABLE `pedidos` (
 --
 
 INSERT INTO `pedidos` (`id`, `nombre`, `cantidad`, `mesa`, `precio`, `hora_pedido`, `listo`) VALUES
-(1390, 'BROWNIE CON HELADO', 1, 1, 7000, '17:28:17', 0);
+(1595, 'ARÓMATICAS', 75, 45, 2500, '15:00:33', 0),
+(1596, 'MILO', 5, 742, 7000, '15:00:36', 0),
+(1599, 'SANDWICH', 75, 88, 12000, '15:00:46', 0),
+(1600, 'ALFAJOR', 75, 44, 3000, '15:00:48', 0),
+(1601, 'CHEESECAKE DE LIMÓN', 57, 75, 7500, '15:00:51', 0),
+(1603, 'CHANTILLY', 5, 45, 3000, '15:00:59', 0),
+(1604, 'HELADO DE VAINILLA', 85, 433, 3000, '15:01:02', 0),
+(1605, 'LECHE DE ALMENDRAS', 64, 465, 3000, '15:01:05', 0),
+(1606, 'WAFFLES', 64, 323, 12000, '15:01:08', 0),
+(1616, 'PORCIÓN DE TORTA (zanahoria-chocolate)', 4, 44, 7000, '16:28:08', 0),
+(1618, 'BROWNIE CON HELADO', 2, 720, 7000, '13:11:55', 0),
+(1619, 'CEVICHE DE PLÁTANO', 1, 742, 10000, '13:12:15', 0),
+(1621, 'LIMONADA DE COCO', 2, 14, 9000, '13:17:02', 0),
+(1626, 'COFFEE TONIC', 1, 6, 10000, '14:20:52', 0),
+(1627, 'COFFEE TONIC', 6, 9, 10000, '14:21:39', 0),
+(1636, 'COFFEE TONIC', 1, 6, 10000, '14:29:43', 0),
+(1653, 'INFUSIONES', 1, 1, 6000, '14:39:13', 0),
+(1654, 'SIROPE DE LA ABUELA', 1, 2, 1000, '14:39:13', 0),
+(1655, 'CHOCOLATE DE LA ABUELA', 1, 1, 6000, '15:38:53', 0);
 
 -- --------------------------------------------------------
 
@@ -115,6 +133,49 @@ INSERT INTO `productos` (`id`, `nombre_producto`, `precio_producto`, `categoria_
 (107, 'HELADO DE VAINILLA', 3000, 4),
 (108, 'PLATANITOS', 3000, 4);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) UNSIGNED NOT NULL,
+  `data` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `sessions`
+--
+
+INSERT INTO `sessions` (`session_id`, `expires`, `data`) VALUES
+('2sc4hcjiK_E_-_fl_4DlaCESlAWYDEZX', 1695166393, '{\"cookie\":{\"originalMaxAge\":60000,\"expires\":\"2023-09-19T23:33:04.941Z\",\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"user\":{\"id\":1,\"nombre\":\"Richi\",\"documento\":\"1002546402\"}}'),
+('OF1-o39Z3Y4wOnT1uTW17f2_k4zNXPb1', 1695166001, '{\"cookie\":{\"originalMaxAge\":60000,\"expires\":\"2023-09-19T23:26:40.654Z\",\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"user\":{\"id\":1,\"nombre\":\"Richi\",\"documento\":\"1002546402\"}}'),
+('OSbXBzkM_priXgPpvvp3RX5zE7_4qbif', 1695166194, '{\"cookie\":{\"originalMaxAge\":60000,\"expires\":\"2023-09-19T23:29:54.126Z\",\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"user\":{\"id\":5,\"nombre\":\"Rosa\",\"documento\":\"1002546403\"}}'),
+('VHEYPKnSTI-ykS8Qd0cprtiut2XCNrOz', 1695166315, '{\"cookie\":{\"originalMaxAge\":60000,\"expires\":\"2023-09-19T23:31:23.254Z\",\"secure\":false,\"httpOnly\":true,\"path\":\"/\"},\"user\":{\"id\":1,\"nombre\":\"Richi\",\"documento\":\"1002546402\"}}');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `documento` varchar(30) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `contrasenia_hash` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `documento`, `nombre`, `contrasenia_hash`) VALUES
+(1, '1002546402', 'Richi', '$2b$10$JzRIpkrd0g/sNF9s0APSJuQSCdaVDByRCq3uTFzetAk61v5BQS.Iq'),
+(5, '1002546403', 'Rosa', '$2b$10$O/J9X16m1B6E7fgV1Y4WteuJekF2.t/0qFqhXCEDR.nBOE7.xTdg.');
+
 --
 -- Índices para tablas volcadas
 --
@@ -139,6 +200,19 @@ ALTER TABLE `productos`
   ADD KEY `categoria_id` (`categoria_id`);
 
 --
+-- Indices de la tabla `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`session_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `documento` (`documento`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -152,13 +226,19 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de la tabla `pedidos`
 --
 ALTER TABLE `pedidos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1391;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1656;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
