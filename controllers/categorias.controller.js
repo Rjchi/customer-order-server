624fac177b52aec1afd4dc49680f7841151d4a80
Rjchi <richardospina18@gmail.com>
2023-10-02 13:55:33 -0500

    [Feat] Agregar crud para las categorias del menú

--- controllers/categorias.controller.js
+++ controllers/categorias.controller.js
@@ -15,6 +15,75 @@ const getCategories = async (req, res) => {
   }
 };
 
+const createCategory = async (req, res) => {
+  try {
+    const [result] = await pool.query(
+      "INSERT INTO categorias(nombre_categoria) VALUES (?)",
+      [req.body.nombre_categoria]
+    );
+
+    if (result.affectedRows !== 0) {
+      return res.sendStatus(204);
+    } else {
+      return res
+        .status(500)
+        .json({ message: `Error al crear una nueva categoria.` });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const updateCategory = async (req, res) => {
+  try {
+    const category = await pool.query("SELECT * FROM categorias WHERE id = ?", [
+      req.body.id,
+    ]);
+
+    if (category[0].length !== 0) {
+      const [result] = await pool.query(
+        "UPDATE categorias SET nombre_categoria = ? WHERE id = ?",
+        [req.body.nombre_categoria_up, req.body.id]
+      );
+
+      if (result.affectedRows !== 0) {
+        return res.sendStatus(204);
+      } else {
+        return res
+          .status(500)
+          .json({ message: `Error al actualizar la categoria.` });
+      }
+    } else {
+      return res
+        .status(404)
+        .json({ message: `No existe la categoria con ese id.` });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteCategory = async (req, res) => {
+  try {
+    const [result] = await pool.query("DELETE FROM categorias WHERE id = ?", [
+      req.body.id,
+    ]);
+
+    if (result.affectedRows !== 0) {
+      return res.sendStatus(204);
+    } else {
+      return res
+        .status(404)
+        .json({ message: `No se encontro la categoria con ese id.` });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   getCategories,
+  createCategory,
+  updateCategory,
+  deleteCategory,
 };
--- routes/categorias.routes.js
+++ routes/categorias.routes.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
-const categorias = require("../controllers/categorias.controller.js")
+const categorias = require("../controllers/categorias.controller.js");
 
 const router = Router();
 
-router.get("/api/get-categories", categorias.getCategories);
+router
+  .get("/api/categories", categorias.getCategories)
+  .post("/api/categories", categorias.createCategory)
+  .put("/api/categories", categorias.updateCategory)
+  .delete("/api/categories", categorias.deleteCategory);
 
 module.exports = router;
