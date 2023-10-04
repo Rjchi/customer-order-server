7af4ad561396b8003eca0e93dfb06e509dd46947
Rjchi <richardospina18@gmail.com>
2023-10-04 13:51:52 -0500

    Eliminar ruta de prueba para la validación de la cookie de sesión

--- routes/validate.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const validateCookie = require("../middlewares/validarCookie.middlewares.js");
-const validate = require("../controllers/validate.controllers.js");
-
-const router = express.Router();
-
-router.get("/validate-cookie", validateCookie, validate.getUserLogged);
-
-module.exports = router;
