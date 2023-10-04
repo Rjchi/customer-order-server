74747b1de2236df1a7ad4439484267a88df35148
Rjchi <richardospina18@gmail.com>
2023-10-04 13:50:43 -0500

    Eliminar middleware de validación de la cookie de sesión

--- middlewares/validarCookie.middlewares.js
+++
@@ -1,8 +0,0 @@
-const validateCookie = (req, res, next) => {
-  if (req.session && req.session.user) {
-    return next();
-  }
-  return res.status(403).send("Acceso prohibido cookie");
-};
-
-module.exports = validateCookie;
