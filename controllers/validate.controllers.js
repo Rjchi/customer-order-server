11163aa5b622516a1933f428a0ec81ff7b2f920e
Rjchi <richardospina18@gmail.com>
2023-10-04 13:52:38 -0500

    Eliminar controlador para la validación de la cookie de sesión

--- controllers/validate.controllers.js
+++
@@ -1,9 +0,0 @@
-const getUserLogged = async (req, res) => {
-  try {
-    return res.json(req.session.user);
-  } catch (error) {
-    return res.status(500).json({ message: error.message });
-  }
-};
-
-module.exports = { getUserLogged };
